f9f0023c617e7b505c72f1922f5d8842ac98addb
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-09-14 22:05:37 +0600

    Added auto-text-effect in home:job and, added details in about page

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 //script
+
+//navigation : mobile icon
 function toggleNav(){
     let icon = document.querySelector('.hamburger');
     icon.classList.toggle('h-change');
     let topnav = document.querySelector('.top-nav');
     topnav.classList.toggle('tnc-change');
+}
+
+//auto text effect in job-rule
+const ruleField = document.getElementById('mcr-rule-title');
+const job = ['Front-end Web Developer', 'Content Creator', 'Programmer'];
+let charIndex = 0;
+let jobIndex = 0;
+function updateRule(){
+    charIndex++;
+    ruleField.innerHTML = job[jobIndex].slice(0, charIndex);
+    if(charIndex===job[jobIndex].length){
+        jobIndex++;
+        charIndex=0;
+    }
+    if(jobIndex===job.length){
+        jobIndex=0;
+    }
+    setTimeout(updateRule, 250);
+}
+updateRule();
+
+//contact button animation
+setTimeout(danceBtn, 2000);
+function danceBtn(){
+    const contantBtn = document.querySelector('.mcr-btn');
+    contantBtn.classList.toggle('mcr-btn-dance');
+    setTimeout(danceBtn, 3000)
 }
