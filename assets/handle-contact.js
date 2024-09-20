c6a951e509eef2972e2739d021e4937fa85fbcae
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-09-20 23:59:40 +0600

    Added contact-form functionalites, used Netlify to handle form and reCaptha

--- assets/handle-contact.js
+++ assets/handle-contact.js
@@ -3,14 +3,18 @@ document.getElementById('send-btn').addEventListener('click', function(event){
     verify(event);
 });
 
+let errorMessage = document.getElementById('error-message');
+
 function verify(event){
     event.preventDefault();
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const message = document.getElementById('message').value.trim();
     if(name === "" || email === "" || message ===""){
-        console.log('all field required');
+        errorMessage.style.visibility = 'visible';
+        errorMessage.textContent = 'All Field are required';
     } else{
-        console.log('ready');
+        errorMessage.style.visibility = 'hidden';
+        document.querySelector('.contact-form').submit();
     }
 }
