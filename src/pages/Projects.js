ffddb0606ce6e2120b30c700b2ec51ffbae9c50c
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-01-26 22:54:59 +0600

    Added blank screen in project images, and improved Project comp by upgrading old html codes into react style

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,6 +13,25 @@ import spPortal from '../projects/student-parent-portal.PNG';
 
 function Projects(){
     useEffect(() => {document.title = 'Projects | Nayem Ahmed'}, []);
+    const hcProjects = [
+        { id: 1, name: 'Profile Card', link: 'https://wev-dev.netlify.app/projects/web-projects/css/profile-card/', ss: profileCard },
+        { id: 2, name: 'Parallax Website', link: 'https://wev-dev.netlify.app/projects/web-projects/css/parallax-website/', ss: parralaxWeb },
+        { id: 3, name: 'Tribute Website', link: 'https://wev-dev.netlify.app/projects/web-projects/css/tribute-website/', ss: tributeWeb },
+        { id: 4, name: 'Furniture Website', link: 'https://wev-dev.netlify.app/projects/furniture-website/', ss: furnitureWeb },
+        { id: 5, name: 'Time Planner App', link: 'https://wev-dev.netlify.app/projects/time-planner-app/', ss: tpaWeb }
+    ];
+    const jsProjects = [
+        { id: 1, name: 'Age Calculator', link: 'https://wev-dev.netlify.app/projects/web-projects/js/life-timer-app/', ss: lifeTimer },
+        { id: 2, name: 'Random Image Generator', link: 'https://wev-dev.netlify.app/projects/web-projects/js/random-image-generator/', ss: randomImg }
+    ];
+    const gameProjects = [
+        { id: 1, name: 'Simple Ludo', link: 'https://simple-ludo-game.netlify.app/', ss: simpleLudo }
+    ];
+    const univProjects = [
+        { id: 1, name: 'Blood Donation MS', link: 'https://uni-project1.netlify.app/', ss: bdmsWeb },
+        { id: 2, name: 'Student-Parent Portal', link: 'https://github.com/nayem-ahmedz/student-parent-portal/', ss: spPortal}
+    ];
+
     return(
         <section>
             <div className="wrapper">
@@ -20,44 +39,26 @@ function Projects(){
                 <p className="text-light text-readable">
                     Since I started learning web development, I tried to build projects, with as much knowledge as I gained. Thanks to some of my guides who taught me to create projects while learning. One says, "Don't just keep learning and sit doing nothing; keep learning and try to build simple projects with whatever you have learned yet." I followed them and I have created lots of projects. Here are a few of them:
                 </p>
-                <article className="css-projects">
+                <article className='css-projects'>
                     <h2 className="project-types-heading">1. HTML & CSS</h2>
                     <select id="sort-projects-1" className="sort-option">
                         <option value="sort">Sort</option>
                         <option value="newer">Newer</option>
                         <option value="older">Older</option>
                     </select>
-                    <div className="project-outer">
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/web-projects/css/profile-card/" target="_blank" rel="noopener noreferrer">
-                                <img src={profileCard} alt="Profile Card screenshot" />
-                            </a>
-                            <h3 className="project-name">Profile Card</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/web-projects/css/parallax-website/" target="_blank" rel="noopener noreferrer">
-                                <img src={parralaxWeb} alt="Parallax Website screenshot" />
-                            </a>
-                            <h3 className="project-name">Parallax Website</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/web-projects/css/tribute-website/" target="_blank" rel="noopener noreferrer">
-                                <img src={tributeWeb} alt="Tribute Website screenshot" />
-                            </a>
-                            <h3 className="project-name">Tribute Website</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/furniture-website/" target="_blank" rel="noopener noreferrer">
-                                <img src={furnitureWeb} alt="Furniture Website screenshot" />
-                            </a>
-                            <h3 className="project-name">Furniture Website</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/time-planner-app/" target="_blank" rel="noopener noreferrer">
-                                <img src={tpaWeb} alt="Time Planner App website screenshot" />
-                            </a>
-                            <h3 className="project-name">Time Planner App</h3>
-                        </div>
+                    <div className='project-outer'>
+                        {
+                            hcProjects.map((el) => {
+                                return(
+                                    <div className='project-inner' key={el.id}>
+                                        <a href={el.link} target='_blank' rel='noopener noreferrer'>
+                                            <img src={el.ss} alt={el.name + ' screenshot'} loading='lazy' />
+                                        </a>
+                                        <h3 className='project-name'> {el.name} </h3>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </article>
                 <article className="js-projects">
@@ -66,19 +67,19 @@ function Projects(){
                         <option value="newer">Newer</option>
                         <option value="older">Older</option>
                     </select>
-                    <div className="project-outer">
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/web-projects/js/life-timer-app/" target="_blank" rel="noopener noreferrer">
-                                <img src={lifeTimer} alt="Life Timer App screenshot" />
-                            </a>
-                            <h3 className="project-name">Age Calculator</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://wev-dev.netlify.app/projects/web-projects/js/random-image-generator/" target="_blank" rel="noopener noreferrer">
-                                <img src={randomImg} alt="Project screenshot" />
-                            </a>
-                            <h3 className="project-name">Random Image Generator</h3>
-                        </div>
+                    <div className='project-outer'>
+                        {
+                            jsProjects.map((el) => {
+                                return(
+                                    <div className='project-inner' key={el.id}>
+                                        <a href={el.link} target='_blank' rel='noopener noreferrer'>
+                                            <img src={el.ss} alt={el.name + ' screenshot'} loading='lazy' />
+                                        </a>
+                                        <h3 className='project-name'> {el.name} </h3>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </article>
                 <article className="game-projects">
@@ -87,13 +88,19 @@ function Projects(){
                         <option value="newer">Newer</option>
                         <option value="older">Older</option>
                     </select>
-                    <div className="project-outer">
-                        <div className="project-inner">
-                            <a href="https://simple-ludo-game.netlify.app/" target="_blank" rel="noopener noreferrer">
-                                <img src={simpleLudo} alt="Simple Ludo screenshot" />
-                            </a>
-                            <h3 className="project-name">Simple Ludo</h3>
-                        </div>
+                    <div className='project-outer'>
+                        {
+                            gameProjects.map((el) => {
+                                return(
+                                    <div className='project-inner' key={el.id}>
+                                        <a href={el.link} target='_blank' rel='noopener noreferrer'>
+                                            <img src={el.ss} alt={el.name + ' screenshot'} loading='lazy' />
+                                        </a>
+                                        <h3 className='project-name'> {el.name} </h3>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </article>
                 <article className="university-projects">
@@ -102,22 +109,19 @@ function Projects(){
                         <option value="newer">Newer</option>
                         <option value="older">Older</option>
                     </select>
-                    <div className="project-outer">
-                        <div className="project-inner">
-                            <a href="https://uni-project1.netlify.app/" target="_blank" rel="noopener noreferrer">
-                                <img src={bdmsWeb} alt="Blood Donation Management System screenshot" />
-                            </a>
-                            <h3 className="project-name">Blood Donation MS</h3>
-                        </div>
-                        <div className="project-inner">
-                            <a href="https://github.com/nayem-ahmedz/student-parent-portal" target="_blank" rel="noopener noreferrer" title="GitHub repository link">
-                                <img src={spPortal} alt="Student-Parent Portal screenshot" />
-                            </a>
-                            <h3 className="project-name">
-                                Student-Parent Portal
-                                <i className="fa-brands fa-github github-link"></i>
-                            </h3>
-                        </div>
+                    <div className='project-outer'>
+                        {
+                            univProjects.map((el) => {
+                                return(
+                                    <div className='project-inner' key={el.id}>
+                                        <a href={el.link} target='_blank' rel='noopener noreferrer'>
+                                            <img src={el.ss} alt={el.name + ' screenshot'} loading='lazy' />
+                                        </a>
+                                        <h3 className='project-name'> {el.name} { el.id === 2 && <i className="fa-brands fa-github github-link"></i> } </h3>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </article>
             </div>
